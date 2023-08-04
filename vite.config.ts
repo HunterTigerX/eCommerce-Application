ab3f50622245a8437c6d81a111ffd0ed905ae214
HunterTigerX <HunterTigerX@gmail.com>
2023-08-04 04:15:25 +0300

    feat: installed basic Typescript SDK, dependencies, updated settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,44 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  if (command === "build") {
+  if (command === 'build') {
     return {
-      mode: "production",
+      mode: 'production',
       plugins: [react()],
     };
   } else {
     return {
-      mode: "dev",
+      mode: 'dev',
       plugins: [react()],
+      resolve: {
+        alias: {
+          stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+        },
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+          define: {
+            global: 'globalThis',
+          },
+          plugins: [
+            NodeGlobalsPolyfillPlugin({
+              process: true,
+              buffer: true,
+            }),
+            NodeModulesPolyfillPlugin(),
+          ],
+        },
+      },
+      build: {
+        rollupOptions: {
+          plugins: [rollupNodePolyFill()],
+        },
+      },
     };
   }
 });
