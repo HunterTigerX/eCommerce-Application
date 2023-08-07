8b1ac03901bc1c94e97a8b7761599fc23a4c8b5b
sidorenko konstantin <kosiak5587@gmail.com>
2023-08-07 14:29:02 +0600

    chore: add Path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  if (command === "build") {
+  if (command === 'build') {
     return {
-      mode: "production",
-      plugins: [react()],
+      mode: 'production',
+      plugins: [react(), tsconfigPaths()],
     };
   } else {
     return {
-      mode: "dev",
-      plugins: [react()],
+      mode: 'dev',
+      plugins: [react(), tsconfigPaths()],
     };
   }
 });
