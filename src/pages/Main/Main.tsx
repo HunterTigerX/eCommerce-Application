0ec9ef1484143d8cb4154868ede3b8832e66d750
sidorenko konstantin <kosiak5587@gmail.com>
2023-08-19 20:18:30 +0600

    feat: add links for cross check

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,59 @@
-import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { message } from 'antd';
-import { useAuth } from '@shared/hooks';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Menu, MenuProps, message } from 'antd';
+// import { useAuth } from '@shared/hooks';
+import {
+  HomeOutlined,
+  ProfileOutlined,
+  ReadOutlined,
+  ShoppingOutlined,
+  UserAddOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const items: MenuProps['items'] = [
+  {
+    label: <Link to="/">Home</Link>,
+    key: 'mail',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to="catalog">Catalog</Link>,
+    key: 'Catalog',
+    icon: <ShoppingOutlined />,
+  },
+  {
+    label: <Link to="about">About</Link>,
+    key: 'about',
+    icon: <ReadOutlined />,
+  },
+  {
+    label: <Link to="/signin">Sign In</Link>,
+    key: 'signin',
+    icon: <UserOutlined />,
+  },
+  {
+    label: <Link to="/signup">Sign Up</Link>,
+    key: 'signup',
+    icon: <UserAddOutlined />,
+  },
+  {
+    label: <Link to="/profile">Profile</Link>,
+    key: 'Profile',
+    icon: <ProfileOutlined />,
+  },
+];
 
 export const Main = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
   const [messageApi, contextHolder] = message.useMessage({ maxCount: 1 });
-  const { user } = useAuth();
+  const [current, setCurrent] = useState('mail');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
 
   const hi = state?.hi;
   const bye = state?.bye;
@@ -35,10 +81,11 @@ export const Main = () => {
   // username: 'te145431323mp555da@mail.ru', password: 'test'
 
   return (
-    <div style={{ color: '#000' }}>
+    <div style={{ color: '#000', width: '300px' }}>
       <h2>Main page</h2>
       {contextHolder}
-      {user && <pre>{JSON.stringify(user, null, 2)}</pre>}
+      <Menu onClick={onClick} selectedKeys={[current]} mode="vertical" items={items} />
+      {/* {user && <pre>{JSON.stringify(user, null, 2)}</pre>} */}
     </div>
   );
 };
