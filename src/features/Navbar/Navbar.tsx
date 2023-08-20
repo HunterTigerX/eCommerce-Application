106ff7acd9084b081d6b88a2b03a411806f5e50b
sidorenko konstantin <kosiak5587@gmail.com>
2023-08-20 20:50:27 +0600

    perf: delete EventListener, update import

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react';
-import { UserMenu } from './ui/UserMenu';
-import { NavList } from './ui/NavList';
-import { Logo } from './ui/Logo';
-import { BurgerMenu } from './ui/BurgerMenu';
+import { BurgerMenu, Logo, NavList, UserMenu } from './ui';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
@@ -21,27 +18,12 @@ export const Navbar = () => {
   const navMenuClass = isMenuClicked ? `${styles.navMenu} ${styles.open}` : styles.navMenu;
 
   useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent | TouchEvent) => {
-      const target = event.target as HTMLElement;
-      if (!target.closest(`.${styles.navbar}`)) {
-        setIsOverflowHidden(false);
-        setIsMenuClicked(false);
-      }
-    };
-
     if (isOverflowHidden) {
       document.body.classList.add('overflow-hidden');
     } else {
       document.body.classList.remove('overflow-hidden');
     }
-    if (isMenuClicked) {
-      document.body.addEventListener('click', handleOutsideClick);
-    }
-
-    return () => {
-      document.body.removeEventListener('click', handleOutsideClick);
-    };
-  }, [isMenuClicked, isOverflowHidden]);
+  }, [isOverflowHidden]);
 
   return (
     <div className={styles.navbar}>
@@ -50,6 +32,7 @@ export const Navbar = () => {
         <NavList onCloseMenu={handleMenuClose} />
         <UserMenu onCloseMenu={handleMenuClose} />
       </div>
+      {isMenuClicked && <div className={styles.backgroundMenu} onClick={handleMenuClose}></div>}
       <BurgerMenu isMenuClicked={isMenuClicked} onBurgerClick={handleMenuClick} />
     </div>
   );
