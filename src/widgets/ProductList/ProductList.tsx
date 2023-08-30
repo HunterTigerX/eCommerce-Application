2fb009a20dbb24d8e7f97273361fdd99d53f1e4e
sidorenko konstantin <kosiak5587@gmail.com>
2023-08-30 17:07:27 +0600

    feat:ET-144 add Products List

---
+++ src/widgets/ProductList/ProductList.tsx
@@ -0,0 +1,25 @@
+import { Spin } from 'antd';
+import { ProductCard } from './ui';
+import { useProductProjections } from '@shared/api/products';
+import styles from './ProductList.module.css';
+
+export const ProductList = () => {
+  const {
+    state: { products, loading: productsLoading },
+  } = useProductProjections();
+
+  return (
+    <>
+      {productsLoading && <Spin tip="Loading" size="large" />}
+      {products && (
+        <>
+          <div className={styles.produckListContainer}>
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
