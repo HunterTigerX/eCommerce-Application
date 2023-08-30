10e99990dee22ca66524dbbc737da59f5ae528f9
sidorenko konstantin <kosiak5587@gmail.com>
2023-08-30 17:04:19 +0600

    feat:ET-143 add Product Card

---
+++ src/widgets/ProductList/ui/ProductCard.tsx
@@ -0,0 +1,55 @@
+import { EuroCircleOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import style from './ProductCard.module.css';
+
+interface ProductCardMap {
+  id: string;
+  title: string;
+  description: string | null;
+  price: number | null;
+  discount: number | null;
+  urlImg: string;
+}
+
+export const ProductCard = ({ product }: { product: ProductCardMap }) => {
+  const { id, title, description, urlImg, price, discount } = product;
+
+  return (
+    <Link to={`/product/${id}`} className={style.productCard}>
+      <div className={style.productImg}>
+        <img src={urlImg} alt={title} />
+      </div>
+      <div className={style.productInfo}>
+        <h4>{title}</h4>
+        {description && <p>{description}</p>}
+        <div className={style.buy}>
+          {price && (
+            <div className={style.productPrice}>
+              {discount ? (
+                <>
+                  <small>
+                    <span>
+                      <EuroCircleOutlined />
+                    </span>{' '}
+                    {price}
+                  </small>
+                  <span>
+                    <EuroCircleOutlined />
+                  </span>{' '}
+                  <strong>{discount}</strong>
+                </>
+              ) : (
+                <span>
+                  <span>
+                    <EuroCircleOutlined />
+                  </span>{' '}
+                  {price}
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
