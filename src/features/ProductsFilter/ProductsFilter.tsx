01389b45d4ff4362b5d67c78e749e95b2b0cc767
sidorenko konstantin <kosiak5587@gmail.com>
2023-09-01 15:06:53 +0600

    feat:ET-145 add countFilter

--- src/features/ProductsFilter/ProductsFilter.tsx
+++ src/features/ProductsFilter/ProductsFilter.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AutoComplete, Button, Checkbox, Drawer, Select, Slider } from 'antd';
+import { AutoComplete, Badge, Button, Checkbox, Drawer, Select, Slider } from 'antd';
 import Title from 'antd/es/typography/Title';
 import { FilterOutlined } from '@ant-design/icons';
 import { CheckboxValueType } from 'antd/es/checkbox/Group';
@@ -19,10 +19,11 @@ const optionsSize = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
 
 export const ProductsFilter = ({ onSearch, onSelect, suggestions, onChange, onClear }: AutoCompleteFilterProps) => {
   const [open, setOpen] = useState(false);
-  const [priceRange, setPriceRange] = useState<[number, number]>([0, 99999]);
+  const [priceRange, setPriceRange] = useState<[number, number]>([0, 9999]);
   const [checkedColorList, setCheckedColorList] = useState<CheckboxValueType[]>([]);
   const [checkedSizeList, setCheckedSizeList] = useState<CheckboxValueType[]>([]);
   const [selectedSort, setSelectedSort] = useState<string | undefined>(undefined);
+  const [countFilters, setCountFilters] = useState(0);
 
   const handleSearch = (text: string) => {
     onSearch(text);
@@ -56,12 +57,26 @@ export const ProductsFilter = ({ onSearch, onSelect, suggestions, onChange, onCl
     setCheckedSizeList(list);
   };
 
+  const countFilter = (reset?: boolean) => {
+    if (reset) return setCountFilters(0);
+    const colorCount = checkedColorList.length;
+    const sizeCount = checkedSizeList.length;
+    let count = colorCount + sizeCount;
+    if (priceRange[0] !== 0 || priceRange[1] !== 9999) {
+      count += 1;
+      setCountFilters(count);
+    } else {
+      setCountFilters(count);
+    }
+  };
+
   const clearFilters = () => {
-    setSelectedSort(undefined);
     setCheckedColorList([]);
     setCheckedSizeList([]);
-    setPriceRange([0, 99999]);
+    setPriceRange([0, 9999]);
     onClear();
+    countFilter(true);
+    setOpen(false);
   };
 
   const applyFilters = () => {
@@ -71,7 +86,8 @@ export const ProductsFilter = ({ onSearch, onSelect, suggestions, onChange, onCl
       size: checkedSizeList,
     };
     console.log(filterParameters);
-
+    countFilter();
+    setOpen(false);
     return filterParameters;
   };
 
@@ -108,20 +124,26 @@ export const ProductsFilter = ({ onSearch, onSelect, suggestions, onChange, onCl
                 value: 'name desc',
                 label: 'Name z-a',
               },
+              {
+                value: 'reset',
+                label: 'Reset',
+              },
             ]}
           />
-          <Button icon={<FilterOutlined />} onClick={showDrawer}>
-            Filter
-          </Button>
+          <Badge count={countFilters}>
+            <Button icon={<FilterOutlined />} onClick={showDrawer}>
+              Filter
+            </Button>
+          </Badge>
           <Drawer title="Filter" placement="right" onClose={onClose} open={open}>
             <div className={styles.filterSection}>
               <Title level={4}>Price</Title>
               <Slider
                 range
-                marks={{ 0: '€0', 99999: '€99999' }}
+                marks={{ 0: '€0', 9999: '€9999' }}
                 value={priceRange}
                 min={0}
-                max={99999}
+                max={9999}
                 onChange={handlePriceChange}
               />
             </div>
