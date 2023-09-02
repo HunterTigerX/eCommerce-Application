71c7573628fa9a65f9de1eb92fb38de7bcc6cfe1
h3nnessey <87146694+h3nnessey@users.noreply.github.com>
2023-09-03 01:01:34 +0500

    feat: ET-137 add useProduct hook, refactor categories component

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,23 +1,19 @@
-import { useProductProjections, ProductProjectionsActionTypes } from '@shared/api/products';
+import { useProductProjections } from '@shared/api/products';
 import { ProductList } from '@widgets/ProductList';
 import { Categories } from '@widgets/Categories';
-import { type Key } from 'rc-tree/lib/interface';
 import { ProductsFilter } from '@features/ProductsFilter';
 
 const Catalog = () => {
   const {
     state: { products, loading },
-    dispatch: setProducts,
+    dispatch,
   } = useProductProjections();
 
   return (
     <>
       <h2>Catalog Products</h2>
-      <ProductsFilter dispatch={setProducts} />
-      <Categories
-        loading={loading}
-        onSelect={(id: Key) => setProducts({ type: ProductProjectionsActionTypes.SET_CATEGORY, payload: id })}
-      />
+      <ProductsFilter dispatch={dispatch} />
+      <Categories loading={loading} dispatch={dispatch} />
       {products && (
         <div style={{ marginTop: '1.5rem' }}>
           <ProductList products={products} loading={loading} />
--- src/widgets/Categories/Categories.tsx
+++ src/widgets/Categories/Categories.tsx
@@ -1,99 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Drawer, Tree } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
-import { Category, CategoryReference } from '@commercetools/platform-sdk';
-import { ApiClient } from '@app/auth/client';
-import { useApiRequest } from '@shared/hooks';
 import { type Key } from 'rc-tree/lib/interface';
+import { ProductProjectionsActionTypes, type ProductProjectionsQueryArgsActions } from '@shared/api/products';
+import { useCategories } from '@shared/api/categories/useCategories';
 
-interface CategoryTreeNode {
-  key: string; // id
-  title: string; // name
-  parent: string | null;
-  path: string;
-  children: CategoryTreeNode[];
+interface CategoriesProps {
+  dispatch: React.Dispatch<ProductProjectionsQueryArgsActions>;
+  loading: boolean;
 }
 
-const getFullPath = (category: Category, categories: Category[], separator = ' / ') => {
-  let result = category.name.en;
-  let root: Category | undefined;
-
-  if (!category.parent) return result;
-
-  let current: CategoryReference | undefined = category.parent;
-
-  while (current) {
-    if (current.obj) {
-      result += separator + current.obj.name.en;
-
-      current = current.obj.parent;
-    } else {
-      const id = current.id;
-
-      root = categories.find((item) => item.id === id);
-
-      current = undefined;
-    }
-  }
-
-  if (root) {
-    result += separator + root.name.en;
-  }
-
-  return result.split(separator).reverse().join(separator);
-};
-
-type CategoriesTreeNodesRecord = Record<string, CategoryTreeNode>;
-
-const mapCategories = (categories: Category[]): CategoriesTreeNodesRecord => {
-  return categories
-    .map((category) => {
-      return {
-        key: category.id,
-        title: category.name.en,
-        parent: category.parent?.id || null,
-        path: getFullPath(category, categories),
-        children: [],
-      };
-    })
-    .reduce(
-      (acc, node) => ({
-        ...acc,
-        [node.key]: node,
-      }),
-      {} as CategoriesTreeNodesRecord
-    );
-};
-
-const getCategoriesTree = (categories: CategoriesTreeNodesRecord) => {
-  const tree: CategoryTreeNode[] = [];
-
-  for (const value in categories) {
-    const node = categories[value];
-    const parent = node.parent;
-
-    if (parent) {
-      categories[parent].children.push(node);
-    } else {
-      tree.push(node);
-    }
-  }
-
-  return tree;
-};
-
-const getAllCategoriesRequest = ApiClient.getInstance()
-  .requestBuilder.categories()
-  .get({
-    queryArgs: {
-      limit: 500,
-      expand: ['parent'],
-    },
-  });
-
-const Categories = ({ onSelect, loading }: { onSelect: (id: Key) => void; loading: boolean }) => {
-  const { data } = useApiRequest(getAllCategoriesRequest);
-  const [treeData, setTreeData] = useState<CategoryTreeNode[]>([]);
+const Categories = ({ dispatch, loading }: CategoriesProps) => {
+  const { categoriesTree } = useCategories();
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
@@ -105,17 +23,8 @@ const Categories = ({ onSelect, loading }: { onSelect: (id: Key) => void; loadin
   };
 
   const onChange = (selected: Key[]) => {
-    const id = selected[0];
-    onSelect(id);
+    dispatch({ type: ProductProjectionsActionTypes.SET_CATEGORY, payload: selected[0] });
   };
-
-  useEffect(() => {
-    if (data) {
-      const mappedCategories = mapCategories(data.results);
-      const tree = getCategoriesTree(mappedCategories);
-      setTreeData(tree);
-    }
-  }, [data]);
 
   return (
     <>
@@ -129,7 +38,7 @@ const Categories = ({ onSelect, loading }: { onSelect: (id: Key) => void; loadin
           style={{ width: 500 }}
           switcherIcon={<DownOutlined />}
           onSelect={onChange}
-          treeData={treeData}
+          treeData={categoriesTree}
         />
       </Drawer>
     </>
